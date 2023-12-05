26e28e5f1d2a20a049ac030fa68a7ce1f681ed48
Developer1 <developer1@Developer1s-MacBook-Pro.local>
2023-12-05 15:51:15 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+
+    <div >
+      <HelloWorld anthorMsg="Hello" msg=" welcome to  vue App"/>
+    </div>
+</template>
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+console.log("HelloWorld:",HelloWorld)
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <h3>{{ anthorMsg }}</h3>
+        <!-- <h5>{{ count }}</h5> -->
+        <button @click="increment()">Increment</button>
+    </div>
+</template>
+
+<script setup>
+// import {ref} from "vue"
+
+defineProps(["msg","anthorMsg"]);
+// let count=ref(0)
+// console.log(count,'valuecoutn')
+// const increment=()=>{
+//     count.value++;
+//     console.log("count",count.value)
+// }
+
+    
+</script>
+
+<style scoped>
+</style>
